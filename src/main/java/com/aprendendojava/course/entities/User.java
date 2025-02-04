d331f56645df7be3f43105ce97bc3f5db3240675
JocelioNobre1983 <JocelioNobre1983>
2025-02-04 20:17:57 -0300

    H2 database, test profile, JPA

--- src/main/java/com/aprendendojava/course/entities/User.java
+++ src/main/java/com/aprendendojava/course/entities/User.java
@@ -3,9 +3,19 @@ package com.aprendendojava.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id//falar pro jpa que o id é a chave primaria do bd
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//ativar autoincremento
 	private Long id;
 	private String name;
 	private String email;
